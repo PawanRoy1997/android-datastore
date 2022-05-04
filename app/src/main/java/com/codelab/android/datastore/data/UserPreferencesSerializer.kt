3f843649ec6a91d89385eacc2ffee1a1df0cdc87
PawanRoy1997 <pawanroy0066@gmail.com>
2022-05-04 12:01:32 +0530

    Error Handling in UserPreferencesSerializer.kt

--- app/src/main/java/com/codelab/android/datastore/data/UserPreferencesSerializer.kt
+++ app/src/main/java/com/codelab/android/datastore/data/UserPreferencesSerializer.kt
@@ -1,15 +1,21 @@
 package com.codelab.android.datastore.data
 
+import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
 import com.codelab.android.database.UserPreferences
+import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
 
-object UserPreferencesSerializer: Serializer<UserPreferences> {
+object UserPreferencesSerializer : Serializer<UserPreferences> {
     override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
 
     override suspend fun readFrom(input: InputStream): UserPreferences {
-        return UserPreferences.parseFrom(input)
+        return try {
+            UserPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
     }
 
     override suspend fun writeTo(t: UserPreferences, output: OutputStream) {
